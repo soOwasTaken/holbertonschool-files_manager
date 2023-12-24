e4866fe4e52fb96516378c92a91a10753573e3c8
soO <444deph12@gmail.com>
2023-12-24 10:55:02 +0000

    server updated for task 2

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ import routes from './routes/index';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Add this line to handle JSON payloads
-app.use(express.json());
-
 app.use('/', routes);
 
 app.listen(PORT, () => {
