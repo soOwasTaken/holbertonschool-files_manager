478f9b182f82c4fe5bd212c490bbd39c0598ac38
soO <444deph12@gmail.com>
2023-12-24 10:28:26 +0000

    task 3

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-import express from "express";
-import routes from "./routes/index.js";
+import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/", routes);
+// Add this line to handle JSON payloads
+app.use(express.json());
+
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
