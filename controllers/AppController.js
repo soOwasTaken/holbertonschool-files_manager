bac016eb07344c74f1a3ac8bd2b012634f88da7d
soO <444deph12@gmail.com>
2023-12-24 10:25:16 +0000

    task3

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+    return res.status(200).send({ redis: redisAlive, db: dbAlive });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    return res.status(200).send({ users, files });
+  }
+}
+
+export default AppController;
